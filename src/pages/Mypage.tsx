a539a60250af3b393855822fbf2d002a442eee3b
childevnote <daniel011125@gmail.com>
2023-03-28 17:01:39 +0900

    Update Mypage.tsx

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,17 +1,24 @@
-import {CartItemList} from "../components/Cart";
+import { CartItemList } from "../components/Cart";
 import React from "react";
 import data from "../data/SampleFood.json";
-import {PageTemplate} from "./PageTemplate";
-import {Typography} from "@mui/material";
+import { PageTemplate } from "./PageTemplate";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 
 export const Mypage: React.FC = () => {
     return (
         <PageTemplate>
-           <div>
-           <Typography variant={"h6"}>마이페이지</Typography>
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
 
-           </div>
+                <Typography variant={"h6"} fontWeight={"bold"}>
+                    <ArrowBackIosNewIcon fontSize="small" /> 마이페이지</Typography>
+                <div style={{ display: "inherit", alignItems: "center", color: "grey" }}>
+                    <a href = "">내 정보 수정</a>
+                </div>
+
+            </div>
         </PageTemplate>
     )
 }
