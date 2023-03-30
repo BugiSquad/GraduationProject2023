a65905993cf2c46003860ee13fadc566136097f4
MenaceOneFive <l2395@naver.com>
2023-03-30 11:57:53 +0900

    브랜치 통합 작업을 위한 사전 커밋

--- src/pages/Matching.tsx
+++ src/pages/Matching.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {faker} from "@faker-js/faker"
 import {Post} from "../components/Post";
-import {Avatar, IconButton, Typography} from "@mui/material";
+import {IconButton, Typography} from "@mui/material";
 import {AddCircle, FilterAlt} from "@mui/icons-material";
 import {PageTemplate} from "./PageTemplate";
+import {MatchingFilter} from "./MatchingFilter";
+import LocalDiningIcon from "@mui/icons-material/LocalDining";
 
 export const Matching: React.FC = () => {
     return (<PageTemplate param={{variant: "WithName", pageHeaderName: "매칭", showBackButton: true}}>
@@ -15,7 +17,7 @@ const Contents: React.FC = () => {
         <div style={{display: "flex", flexDirection: "column", width: "90%"}}>
             <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}>
                 <div style={{display: "inherit", alignItems: "center"}}>
-                    <Avatar src={"https://www.svgrepo.com/download/265489/tray-dinner.svg"}/>
+                    <LocalDiningIcon/>
                     <Typography variant={"h6"} fontWeight={"bold"}>같이 먹어요!</Typography>
                 </div>
                 <div style={{display: "inherit", alignItems: "center"}}>
@@ -23,6 +25,7 @@ const Contents: React.FC = () => {
                     <IconButton><FilterAlt style={{color: "orange"}}/></IconButton>
                 </div>
             </div>
+            <MatchingFilter></MatchingFilter>
             <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
                 <Posts/>
             </div>
