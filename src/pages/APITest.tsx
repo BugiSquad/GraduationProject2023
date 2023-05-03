41f0dff1e72852e0f1ba8abc213da2c80a3e7514
childevnote <daniel011125@gmail.com>
2023-05-04 02:07:22 +0900

    addMenus문제 수정

--- src/pages/APITest.tsx
+++ src/pages/APITest.tsx
@@ -57,7 +57,7 @@ export const APITest: React.FC = () => {
             <Typography variant={"h4"}>API테스트</Typography>
             <Button onClick={onButtonClicked}>회원가입</Button>
             {/*<Button onClick={createPost}>글 올리기</Button>*/}
-            <Button onclick={addMenus}>메뉴 등록</Button>
+            <Button  onClick={()=>{addMenus()}}>메뉴 등록</Button>
             <Button onClick={createDummyMembers}>더미 회원 만들기(X100)</Button>
             <Button onClick={createDummyPosts}>더미 글 올리기</Button>
         </div>
