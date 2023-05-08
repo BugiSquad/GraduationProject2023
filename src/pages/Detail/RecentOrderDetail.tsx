b6e074f0c7cc15280871d9a812eb71bed29cf6be
childevnote <daniel011125@gmail.com>
2023-05-08 16:29:09 +0900

    radiobar 컴포넌트화해서 재사용 구현중

--- src/pages/Detail/RecentOrderDetail.tsx
+++ src/pages/Detail/RecentOrderDetail.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { SimpleTemplate} from "../PageTemplate";
+import { SimpleTemplate } from "../PageTemplate";
 import { RecentOrders } from "../../components/RecentOrders";
+import { PageCards } from "../../components/PageCards";
 
 export const RecentOrderDetail: React.FC = () => {
     return (
-        <SimpleTemplate param={{ pageHeaderName: "최근 주문 내역"}}>
-            <div>
-                <RecentOrders />
+        <SimpleTemplate param={{ pageHeaderName: "최근 주문 내역" }}>
+            <div style={{paddingTop:"20px"}}>
+                <PageCards title="조회 조건" content={<></>} />
+                
+                <PageCards title="조회 내역" content={<RecentOrders/>} />
             </div>
         </SimpleTemplate>
     )
