78f86e71f682162493412393f5c0dedbe0046dc8
childevnote <daniel011125@gmail.com>
2023-05-09 12:34:56 +0900

    최근 주문 조회 수정중

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -1,18 +1,26 @@
 import data from "../data/SampleFood.json";
-import {MenuCategories} from "./MenuCategory"
-import {CartItemList} from "./Cart"
-import {MenuItem} from "../types/MenuItem";
+import { CartItemList } from "./Cart"
+import { MenuItem } from "../types/MenuItem";
+import { RadioBarItem, bgColor } from "./RadioBar";
+
 
 const foods: MenuItem[] = data
 
+const categories = [
+    { name: "전체", bgColor: bgColor, selectValue: false },
+    { name: "찌개류", bgColor: bgColor, selectValue: false },
+    { name: "면류", bgColor: bgColor, selectValue: false },
+    { name: "분식", bgColor: bgColor, selectValue: false },
+];
 
 export const MenuList: React.FC = () => {
     return (
         <>
-            <MenuCategories/>
-            <div style={{display: "flex", justifyContent: "center"}}>
+            <RadioBarItem items={categories} />
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
                 <CartItemList items={foods.map((e) => {
-                    return {...e, quantity: 0}
+                    return { ...e, quantity: 0 }
                 })}></CartItemList>
             </div>
         </>
