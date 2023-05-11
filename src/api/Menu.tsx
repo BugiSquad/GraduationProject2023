3636bcdcdd1e73fcc2beb8a7df5904735978684b
MenaceOneFive <l2395@naver.com>
2023-05-11 11:03:30 +0900

    면담 전 UI변경 및 개선

--- src/api/Menu.tsx
+++ src/api/Menu.tsx
@@ -22,6 +22,9 @@ export function getMenuById(id: number) {
  */
 export function addMenus() {
     let ary = JSON.parse(JSON.stringify(data));
+    ary.forEach((item: MenuItem) => {
+        item.imageUrl = "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    })
     ary.forEach((item: any) => {
         axios.post(`${getApiURL()}/menu`, {...item}, {
             headers: {
