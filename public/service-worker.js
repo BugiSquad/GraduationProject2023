d6e61a2bfaa3b4503d166a069186c094f644daec
MenaceOneFive <l2395@naver.com>
2023-05-13 01:29:50 +0900

    워커 수정

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,19 +7,19 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('push', function (event) {
-    // 푸시 메시지 수신 시 실행되는 코드
-    const data = "서버메시지"
+    // const data = event.data.json(); // 푸시 메시지의 데이터를 추출
+    //
+    const options = {
+        body: "도착", // 푸시 메시지 내용
+        // icon: 'icon.png', // 알림 아이콘 URL
+        // 기타 옵션들...
+    };
 
-    event.waitUntil(async function () {
-        for (const client of await self.clients.matchAll({includeUncontrolled: true})) {
-            client.postMessage(data);
-        }
+    event.waitUntil(
+        self.registration.showNotification("빈 제목", options)
+    );
+})
 
-        self.registration.showNotification(data.title, {
-            body: data.message
-        })
-    });
-
-    self.addEventListener('notificationclick', function (event) {
-        // 푸시 알림 클릭 시 실행되는 코드
-    });
+self.addEventListener('notificationclick', function (event) {
+    // 푸시 알림 클릭 시 실행되는 코드
+});
