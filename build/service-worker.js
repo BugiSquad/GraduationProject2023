7cb84c3b3428b4223203820a9faff12681e56f83
MenaceOneFive <l2395@naver.com>
2023-05-13 01:10:27 +0900

    타입에러 수정

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,29 +1,22 @@
-self.addEventListener('install', function (event) {
-    // 서비스 워커 설치 시 실행되는 코드
-});
-
-self.addEventListener('activate', function (event) {
-    // 서비스 워커 활성화 시 실행되는 코드
-});
-
 self.addEventListener('push', function (event) {
-    // 푸시 메시지 수신 시 실행되는 코드
-    const data = {title: "서버메시지", message: "푸시 메시지 내용"};
+    // const data = event.data.json(); // 푸시 메시지의 데이터를 추출
+    //
+    const options = {
+        body: "도착", // 푸시 메시지 내용
+        // icon: 'icon.png', // 알림 아이콘 URL
+        // 기타 옵션들...
+    };
 
     event.waitUntil(
-        (async function () {
-            const clients = await self.clients.matchAll({includeUncontrolled: true});
-            for (const client of clients) {
-                client.postMessage(data);
-            }
-
-            self.registration.showNotification(data.title, {
-                body: data.message
-            });
-        })()
+        self.registration.showNotification("빈 제목", options)
     );
 });
 
 self.addEventListener('notificationclick', function (event) {
-    // 푸시 알림 클릭 시 실행되는 코드
+    event.notification.close(); // 알림 닫기
+
+    // 알림 클릭 시 수행할 동작
+    event.waitUntil(
+        clients.openWindow('https://www.example.com') // 새 창 또는 웹 페이지 열기
+    );
 });
