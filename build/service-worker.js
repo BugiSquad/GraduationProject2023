2a601f49931a4a5476a7b8f3c2e39a1c7a34db3c
MenaceOneFive <l2395@naver.com>
2023-05-12 23:40:02 +0900

    알림기능 테스트

---
+++ build/service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function (event) {
+    // 서비스워커 설치 시 실행되는 코드
+});
+
+self.addEventListener('activate', function (event) {
+    // 서비스워커 활성화 시 실행되는 코드
+});
+
+self.addEventListener('push', function (event) {
+    // 푸시 메시지 수신 시 실행되는 코드
+    const data = "서버메시지"
+
+    event.waitUntil(async function () {
+        for (const client of await self.clients.matchAll({includeUncontrolled: true})) {
+            client.postMessage(data);
+        }
+
+        self.registration.showNotification(data.title, {
+            body: data.message
+        })
+    });
+
+    self.addEventListener('notificationclick', function (event) {
+        // 푸시 알림 클릭 시 실행되는 코드
+    });
+})
