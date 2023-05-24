78f412b21617f9af4bcde717a666e390cda5a066
childevnote <daniel011125@gmail.com>
2023-05-24 11:32:53 +0900

    Update Filter.tsx

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
-import {Button, Card, Typography} from "@mui/material";
-import {InterestFilter, InterestOptionType} from "./InterestFilter";
-import {useDispatch} from "react-redux";
-import {closeFilter} from "../store/matching/drawer";
-import {DrawerGrid, DrawerGridChild} from "./styled/DrawerGrid";
-import {OrangeButton} from "./styled/Buttons";
+import React, { useState } from "react";
+import { Button, Card, Typography } from "@mui/material";
+import { InterestFilter, InterestOptionType } from "./InterestFilter";
+import { useDispatch } from "react-redux";
+import { closeFilter } from "../store/matching/drawer";
+import { DrawerGrid, DrawerGridChild } from "./styled/DrawerGrid";
+import { OrangeButton } from "./styled/Buttons";
 import { normalCard } from "./styled/Cards";
 import { DepartmentsFilter } from "./DepartmentsFilter";
 import { GradeFilter } from "./GradeFilter";
@@ -25,13 +25,13 @@ export const Filter: React.FC = () => {
                 display: "flex", justifyContent: "flex-start", flexDirection: "column", alignItems: "center"
                 , height: "70vh"
             }}>
-                <Card sx={normalCard} style={{width:"100px",marginTop:"10px"}}></Card>
-                <Typography variant={"h5"} fontWeight={"bold"}>🍽️필터</Typography>
+                <Card sx={normalCard} style={{ width: "100px", marginTop: "10px",  }}></Card>
+                <Typography variant={"h5"} fontWeight={"bold"}>🍽️ 필터</Typography>
                 <DrawerGrid container rowSpacing={1}>
                     {/*위로 추가하는 바 필요*/}
 
                     <DrawerGridChild item xs={12}>
-                        <InterestFilter setSelectedInterests={setInterests} selectedInterests={interests}/>
+                        <InterestFilter setSelectedInterests={setInterests} selectedInterests={interests} />
                     </DrawerGridChild>
 
                     <DrawerGridChild item xs={12}>
@@ -48,7 +48,7 @@ export const Filter: React.FC = () => {
 
                     <DrawerGridChild item xs={12}>
                         <Button sx={OrangeButton} onClick={() => onClick()}>
-                            <Typography sx={{color: "white"}}>적용</Typography>
+                            <Typography sx={{ color: "white" }}>적용</Typography>
                         </Button>
                     </DrawerGridChild>
                 </DrawerGrid>
