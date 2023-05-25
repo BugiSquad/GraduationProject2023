062722a06f683c78d54b209d29ab28c0cbcb5fd0
childevnote <daniel011125@gmail.com>
2023-05-25 22:07:42 +0900

    미니로고 추가

--- src/components/PageHeaders/MainPageHeader.tsx
+++ src/components/PageHeaders/MainPageHeader.tsx
@@ -1,12 +1,14 @@
-import {Avatar, Badge} from "@mui/material";
+import { Avatar, Badge } from "@mui/material";
 
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from '../../images/logo.png';
-import {getMyToken} from "../../api/Common";
+import minilogo from '../../images/minilogo.png';
+
+import { getMyToken } from "../../api/Common";
 import profilePic from '../../images/default.png'
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import {useAppSelector} from "../../store/hooks";
+import { useAppSelector } from "../../store/hooks";
 
 interface PageHeaderParam {
     pageHeaderName: String;
@@ -17,26 +19,27 @@ interface PageHeaderParam {
 export const MainPageHeader: React.FC<PageHeaderParam>
     = (pageHeaderParam: PageHeaderParam) => {
 
-    const navigate = useNavigate()
-    const count = useAppSelector(app => app.cart.item.length)
-    return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingBottom: "1rem",
-            paddingLeft: '1rem',
-            paddingRight: '1rem'
-        }}>
-            <Badge badgeContent={count} color="primary">
-                <ShoppingCartIcon onClick={() => navigate("/cart")}></ShoppingCartIcon>
-            </Badge>
-            <Link to={"/app"} onClick={() => {
-            }}><img src={logo} style={{paddingTop: "10px", width: '50%', height: '50%', objectFit: 'cover'}}/></Link>
-            <div style={{display: "inline-block", whiteSpace: "nowrap"}}>
-                {(getMyToken() !== "") ? <Link to={"/mypage"} onClick={() => {
+        const navigate = useNavigate()
+        const count = useAppSelector(app => app.cart.item.length)
+        return (
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingBottom: "1rem",
+                paddingLeft: '1rem',
+                paddingRight: '1rem'
+            }}>
+                <Badge badgeContent={count} color="primary">
+                    <ShoppingCartIcon onClick={() => navigate("/cart")}></ShoppingCartIcon>
+                </Badge>
+                <Link to={"/app"} onClick={() => {
+                }}>            <img src={minilogo} style={{ paddingTop: "10px", width: '18%', objectFit: 'cover' }} />
+                    <img src={logo} style={{ paddingTop: "10px", width: '50%', height: '50%', objectFit: 'cover' }} /></Link>
+                <div style={{ display: "inline-block", whiteSpace: "nowrap" }}>
+                    {(getMyToken() !== "") ? <Link to={"/mypage"} onClick={() => {
                     }}><Avatar src={profilePic}></Avatar></Link> :
-                    <Link to={"/login"} style={{color: "black", fontSize: "14px"}}>로그인</Link>}
-            </div>
-        </div>)
-}
+                        <Link to={"/login"} style={{ color: "black", fontSize: "14px" }}>로그인</Link>}
+                </div>
+            </div>)
+    }
