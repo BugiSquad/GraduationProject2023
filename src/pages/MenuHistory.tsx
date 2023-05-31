0206a7bb64f6d60818691680d2281b1671987688
childevnote <daniel011125@gmail.com>
2023-06-01 00:39:40 +0900

    주문 일시 표현

--- src/pages/MenuHistory.tsx
+++ src/pages/MenuHistory.tsx
@@ -23,13 +23,17 @@ export const MenuHistory: React.FC = () => {
 };
 
 export const MenuHistoryContent: React.FC<{ list: OrderList }> = (props) => {
+
+  const paymentTime = new Date(props.list.paymentDto.paymentTime);
+  const formattedDate = paymentTime.toLocaleDateString('ko-KR',  { year: 'numeric', month: 'long', day: 'numeric', hour:'2-digit', minute:'2-digit', second:'2-digit' });
+
   return (
     <div style={{ width: "100%" }}>
       <PageCards title={"주문 번호"} content={<Typography sx={{...normalTypography}} fontSize={20}>{props.list.ordersId}</Typography>} />
       <PageCards title={"주문 메뉴"} content={<Typography sx={{...normalTypography}} fontSize={20}>{props.list.paymentDto.detail}</Typography>} />
       <PageCards title={"결제수단"} content={<Typography sx={{...normalTypography}} fontSize={20}>{props.list.paymentDto.paymentType}</Typography>} />
       <PageCards title={"합계 금액"} content={<Typography sx={{...normalTypography}} fontSize={20}>{props.list.totalPrice.toLocaleString()}원</Typography>} />
-      <PageCards title={"주문 정보"} content={<Typography sx={{...normalTypography}} fontSize={20}>{props.list.paymentDto.confirmNum}</Typography>} />
+      <PageCards title={"주문 일시"} content={<Typography sx={{...normalTypography}} fontSize={20}>{formattedDate}</Typography>} />
     </div>
   );
 };
