5be7fb64dcb63fac905165e395655402f089fe90
childevnote <daniel011125@gmail.com>
2023-06-01 16:49:40 +0900

    최근 만남 디자인 수정

--- src/pages/RecentMeetInfo.tsx
+++ src/pages/RecentMeetInfo.tsx
@@ -4,7 +4,7 @@ import { BottomNavigationTab } from "../types/PageHeaderParam";
 import { MeetingInfo, MemberMeetingDto } from "../components/RecentMeets";
 import { useLoaderData } from "react-router-dom";
 import { getPromise } from "../api/Promise";
-import { Avatar, Card, Typography } from "@mui/material";
+import { Avatar, Card, Divider, List, Typography } from "@mui/material";
 import { normalTypography } from "../components/styled/Text";
 
 export const RecentMeetInfo: React.FC = () => {
@@ -25,34 +25,35 @@ const Content: React.FC = () => {
 
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", paddingTop:"20px" }}>
+        <List style={{ display: "flex", flexDirection: "column", paddingTop: "10px", margin: "15px" }}>
+            <Divider component="li"  sx={{ marginBottom: "60px" }} />
 
-            <Card sx={{ minHeight: "600px", flexDirection: "column", marginBottom: "1rem", backgroundColor: "white", color: "black", padding: "24px",borderRadius: "1rem", boxShadow: "0px 5px 5px rgba(0, 0, 0, 0.3)" }}>
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-                    <Typography fontWeight={'bold'} fontSize={20}>언제 만났었나요?</Typography>
-                    <Typography variant={"subtitle1"}>{formattedDate}</Typography>
-                </div>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+                <Typography fontWeight={'bold'} fontSize={20}>언제 만났었나요?</Typography>
+                <Typography variant={"subtitle1"}>{formattedDate}</Typography>
+            </div>
+            <Divider component="li" sx={{ marginTop: "60px" }} />
 
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", paddingTop: "100px" }}>
-                    <Typography fontWeight={'bold'} fontSize={20}>어디서 만났었나요?</Typography>
-                    <Typography variant={"subtitle1"}>{info.location}</Typography>
-                </div>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", paddingTop: "60px" }}>
+                <Typography fontWeight={'bold'} fontSize={20}>어디서 만났었나요?</Typography>
+                <Typography variant={"subtitle1"}>{info.location}</Typography>
+            </div>
+            <Divider component="li" sx={{ marginTop: "60px" }} />
 
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", paddingTop: "100px" }}>
-                    <Typography fontWeight={'bold'} fontSize={20}>누구와 만났었나요?</Typography>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", paddingTop: "40px" }}>
+                <Typography fontWeight={'bold'} fontSize={20}>누구와 만났었나요?</Typography>
 
-                    {info.memberMeetingDto.map((item: MemberMeetingDto) => {
-                        return (
-                            <div style={{ display: "flex", alignItems: "center", marginTop: "30px" }}>
-                                <Avatar src={item.profileUrl}></Avatar>
-                                <Typography sx={{ paddingLeft: "20px" }} variant={"subtitle1"}>{item.name}</Typography>
-                            </div>)
-                    })}
-                </div>
+                {info.memberMeetingDto.map((item: MemberMeetingDto) => {
+                    return (
+                        <div style={{ display: "flex", alignItems: "center", marginTop: "30px" }}>
+                            <Avatar src={item.profileUrl}></Avatar>
+                            <Typography sx={{ paddingLeft: "20px" }} variant={"subtitle1"}>{item.name}</Typography>
+                        </div>)
+                })}
+            </div>
+            <Divider component="li"  sx={{ marginTop: "40px" }} />
 
-
-            </Card>
-        </div>
+        </List>
 
 
     )
